79ea37c54ead1dc1c3b986a6b84e2a5d70d9b90e
Chotu Kumar <ck4001252@gmail.com>
2024-12-30 09:12:57 +0530

    few user dashboard changes

--- client/src/user/UserDashboard.jsx
+++ client/src/user/UserDashboard.jsx
@@ -6,6 +6,7 @@ import Navbar from '@/components/ui/shared/Navbar';
 import backgroundImage from '../assets/Images/Herobgi.png'
 import { MdVerified } from "react-icons/md";
 import { useNavigate } from 'react-router-dom';
+import Loading from '@/components/ui/shared/Loading';
 
 const Agents = () => {
   const [agents, setAgents] = useState([]);
@@ -36,7 +37,7 @@ const Agents = () => {
       <div className="mt-24 flex flex-col items-center px-4">
         <h2 className="text-3xl font-bold text-gray-800 mb-8">Available Agents</h2>
         {agents.length === 0 ? (
-          <p className="text-gray-600">No approved agents available at the moment.</p>
+          <Loading/>
         ) : (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-6xl w-full">
             {agents.map(agent => (
